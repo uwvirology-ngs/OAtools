ca1a0fe6b8954b7f27f735e60bfb24c618313dd2
aidants <aidantshea@gmail.com>
2025-08-20 14:35:48 -0700

    feat: add basilisk environment configuration

---
+++ R/basilisk.R
@@ -0,0 +1,6 @@
+OAtools_env <- basilisk::BasiliskEnvironment(
+  envname = "conda_env",
+  pkgname = "OAtools",
+  packages = c("python=3.13.5", "numpy=2.3.2", "pandas=2.3.1", "scipy=1.16.1"),
+  channels = c("conda-forge")
+)
