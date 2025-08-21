14e1336cb08d15956e1b6df90e817cf38798a764
aidants <aidantshea@gmail.com>
2025-08-21 15:26:44 -0700

    test: unit tests for tidy_gene_expression_data
    Introduces unit testing for the tidy_gene_expression_data function.

---
+++ tests/testthat/test-tidy_gene_expression_data.R
@@ -0,0 +1,37 @@
+test_that("tidy_gene_expression_data returns expected results", {
+
+  # create tidy_run_data as in function documentation
+  path = system.file("extdata", "gene_expression_run_data.xlsx", package = "OAtools")
+  result <- tidy_gene_expression_data(path = path, num_results = 96)
+
+  # verify function return type
+  expect_true(tibble::is_tibble(result))
+
+  # test column names
+  expect_equal(
+    colnames(result),
+    c("well", "well_position", "sample_name", "target_name", "crt", "amp_score",
+      "cq_conf", "amp_status", "cycle", "fam", "batch_name")
+  )
+
+  # test column types
+  expected_types <- c(
+    well = "integer",
+    well_position = "factor",
+    sample_name = "factor",
+    target_name = "factor",
+    crt = "numeric",
+    amp_score = "numeric",
+    cq_conf = "numeric",
+    amp_status = "factor",
+    cycle = "integer",
+    fam = "numeric",
+    batch_name = "factor"
+  )
+  observed_types <- sapply(result, function(col) class(col)[1])
+  expect_equal(observed_types, expected_types)
+
+  # ensure result is identical to example package data
+  expect_identical(result, tidy_run_data)
+
+})
