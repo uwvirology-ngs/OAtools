db00aaf426b7d5cc3bc31af1e42d871485384df6
aidants <aidantshea@gmail.com>
2025-08-22 14:56:54 -0700

    feat: add format_results function
    Adds the format_results function, which transforms the core long tibble
    data format into a wider tibble more appropriate for data reporting.

---
+++ R/format_results.R
@@ -0,0 +1,34 @@
+#' Format results for reporting
+#'
+#' Reformats analyzed OpenArray qPCR data, converting the long tidy tibble into a wider,
+#' human-readable one in which each row reflects one reaction. Fluorescence data for each
+#' cycle may be optionally included.
+#'
+#' @param data A tibble containing analyzed qPCR data
+#' @param include_fluorescence_data logical determining whether fluorescence vs. cycle data is included
+#'
+#' @returns A reformatted tibble for reporting
+#'
+#' @importFrom rlang .data
+#'
+#' @export
+#'
+#' @examples
+#' formatted_results <- format_results(data = result_data, include_fluorescence_data = TRUE)
+format_results <- function(data, include_fluorescence_data) {
+
+  if (include_fluorescence_data) {
+    data <- data |>
+      tidyr::pivot_wider(
+        names_from = "cycle",
+        values_from = "fam",
+        names_prefix = "cycle_"
+      )
+  } else {
+    data <- data |>
+      dplyr::distinct(.keep_all = TRUE) |>
+      dplyr::select(-dplyr::matches("fam|cycle"))
+  }
+
+  return(data)
+}
