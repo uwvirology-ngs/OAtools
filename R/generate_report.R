fbd3196ddeb255dcecfd215601d7ac436e836330
aidants <aidantshea@gmail.com>
2025-08-28 13:54:53 -0700

    refactor: paths param for generate_report
    Adds a parameter to generate_report function to specify an outfile path.

--- R/generate_report.R
+++ R/generate_report.R
@@ -3,14 +3,16 @@
 #' Generates a .html report
 #'
 #' @param data a tibble of qPCR data ready for reporting
+#' @param path intended outfile path
 #'
 #' @returns an .html report summarizing the OpenArray run results
 #'
 #' @export
 #'
 #' @examples
-#' generate_report(result_data)
-generate_report <- function(data) {
+#' path = file.path("C:", "Users", "aidants", "Downloads")
+#' generate_report(result_data, path)
+generate_report <- function(data, path) {
 
   data_file <- tempfile(fileext = ".rds")
   saveRDS(data, data_file)
@@ -18,6 +20,7 @@ generate_report <- function(data) {
   rmarkdown::render(
     input = system.file("reports", "pcr_report.rmd", package = "OAtools"),
     output_file = "report.html",
+    output_dir = path,
     params = list(data_path = data_file)
   )
 }
