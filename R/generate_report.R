bf6f535ccbc12e11f71c510597bd079aa39cfa0d
aidants <aidantshea@gmail.com>
2025-08-29 11:06:39 -0700

    feat: pcr_report supports dynamic analysis type
    Adds support for curve-fitting analysis approach to the pcr_report,
    modifies the generate_report function to accomodate.

--- R/generate_report.R
+++ R/generate_report.R
@@ -4,6 +4,7 @@
 #'
 #' @param data a tibble of qPCR data ready for reporting
 #' @param path intended outfile path
+#' @param analysis character string for analytic method, supports 'native' and 'curve-fitting' approaches
 #'
 #' @returns an .html report summarizing the OpenArray run results
 #'
@@ -12,7 +13,7 @@
 #' @examples
 #' path = file.path("C:", "Users", "aidants", "Downloads")
 #' generate_report(result_data, path)
-generate_report <- function(data, path) {
+generate_report <- function(data, path, analysis = 'native') {
 
   data_file <- tempfile(fileext = ".rds")
   saveRDS(data, data_file)
@@ -21,7 +22,7 @@ generate_report <- function(data, path) {
     input = system.file("reports", "pcr_report.rmd", package = "OAtools"),
     output_file = "report.html",
     output_dir = path,
-    params = list(data_path = data_file),
+    params = list(data_path = data_file, analysis = analysis),
     envir = environment()
   )
 }
