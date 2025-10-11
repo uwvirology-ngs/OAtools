d88cd1efd2b0f441b1281584e9659d6939a1947b
Aidan Shea <aidantshea@gmail.com>
2025-10-10 19:55:14 -0700

    test: core pipeline unit tests
    Revamps unit testing for append_fit_results and assign_calls_with_key
    functions. Coverage now extends to fit quality and correct results.
    Removed test for equality to package data objects for maintainability
    and switched to a far smaller test dataset for faster test runtime.
    Removes the format_results unit tests.

--- tests/testthat/test-append_fit_results.R
+++ tests/testthat/test-append_fit_results.R
@@ -1,18 +1,22 @@
-test_that("append_fit_results returns expected results", {
+test_that("append_fit_results has expected return type and acceptably fits 5pl model", {
 
-  # create tidy_run_data as in function documentation
-  targets <- c("S. pneumoniae_Ba06439619_s1", "RNAse_P_Pa04930436_g1")
-  data <- tidy_run_data_cumulative |> dplyr::filter(target_name %in% targets)
-  result <- append_fit_results(data = data, linear_threshold = 400)
+  # generate fit data for unit testing
+  path <- system.file("extdata", "oa_gene_expression_batch1.xlsx", package = "OAtools")
+  targets <- c("RV_1of2_Vi99990016_po", "RNAse_P_Pa04930436_g1", "S. pneumoniae_Ba06439619_s1")
+  linear_threshold <- 400
+  
+  result <- tidy_gene_expression_data(path = path, num_results = 96) |> 
+    dplyr::filter(sample_name == "Sample-102" & target_name %in% targets) |> 
+    append_fit_results(linear_threshold = linear_threshold)
 
-  # verify function return type
+  # validate return type
   expect_true(tibble::is_tibble(result))
 
-  # test column names
-  new_cols <- c("regression_type", "x_mid", "slope", "delta")
-  expect_true(all(new_cols %in% colnames(result)))
+  # validate column names
+  expected_cols <- c("regression_type", "x_mid", "slope", "delta")
+  expect_true(all(expected_cols %in% colnames(result)))
 
-  # test column types
+  # validate column types
   expected_types <- c(
     regression_type = "character",
     x_mid = "numeric",
@@ -21,8 +25,18 @@ test_that("append_fit_results returns expected results", {
   )
   observed_types <- sapply(result, function(col) class(col)[1])
   expect_equal(observed_types[names(expected_types)], expected_types)
-
-  # ensure result is equivalent to example package data
-  expect_equal(result, curve_fit_data, tolerance = 1e-3)
-
+  
+  # validate regression type
+  df <- result |> dplyr::distinct(well, batch_name, .keep_all = TRUE)
+  expect_true(all(
+    (df$delta >= 400 & df$regression_type == "5pl") | 
+    (df$delta < 400 & df$regression_type == "lin")
+  ))
+  
+  # ensure quality of 5pl fit
+  df <- result |> dplyr::filter(well == "2385")
+  mse <- mean((df$fam - df$fam_pred)^2)
+  expect_true(all(df$regression_type == "5pl"))
+  expect_lt(mse, 2500)
+  
 })
--- tests/testthat/test-assign_calls_with_key.R
+++ tests/testthat/test-assign_calls_with_key.R
@@ -1,30 +1,40 @@
-test_that("assign_calls_with_key returns expected results", {
-
-  # create tidy_run_data as in function documentation
-  key_path = system.file("extdata", "target_threshold_key.xlsx", package = "OAtools")
-  result <- assign_calls_with_key(data = curve_fit_data, key_path = key_path)
-
-  # verify function return type
-  expect_true(tibble::is_tibble(result))
-
-  # test column names
-  new_cols <- c("slope_threshold", "delta_threshold", "crt_threshold", "slope_pass", "delta_pass", "crt_pass", "result")
-  expect_true(all(new_cols %in% colnames(result)))
-
-  # test column types
-  expected_types <- c(
-    slope_threshold = "numeric",
-    delta_threshold = "numeric",
-    crt_threshold = "numeric",
-    slope_pass = "logical",
-    delta_pass = "logical",
-    crt_pass = "logical",
-    result = "character"
-  )
-  observed_types <- sapply(result, function(col) class(col)[1])
-  expect_equal(observed_types[names(expected_types)], expected_types)
-
-  # ensure result is identical to example package data
-  expect_equal(result, result_data, tolerance = 1.e-3)
-
-})
+  test_that("assign_calls_with_key returns expected results", {
+  
+    # assign_calls_with_key has expected return type
+    data_path <- system.file("extdata", "oa_gene_expression_batch1.xlsx", package = "OAtools")
+    key_path <- system.file("extdata", "target_threshold_key.xlsx", package = "OAtools")
+    targets <- c("RV_1of2_Vi99990016_po", "S. pneumoniae_Ba06439619_s1")
+    
+    result <- tidy_gene_expression_data(path = data_path, num_results = 96) |> 
+      dplyr::filter(sample_name == "Sample-102") |> 
+      dplyr::filter(target_name %in% targets) |> 
+      append_fit_results(linear_threshold = 400) |> 
+      assign_calls_with_key(key_path = key_path)
+  
+    # validate return type
+    expect_true(tibble::is_tibble(result))
+  
+    # validate column names
+    new_cols <- c("slope_threshold", "delta_threshold", "crt_threshold", "slope_pass", "delta_pass", "crt_pass", "result")
+    expect_true(all(new_cols %in% colnames(result)))
+  
+    # validate column types
+    expected_types <- c(
+      slope_threshold = "numeric",
+      delta_threshold = "numeric",
+      crt_threshold = "numeric",
+      slope_pass = "logical",
+      delta_pass = "logical",
+      crt_pass = "logical",
+      result = "character"
+    )
+    observed_types <- sapply(result, function(col) class(col)[1])
+    expect_equal(observed_types[names(expected_types)], expected_types)
+    
+    # confirm wells are assigned correct results
+    expect_true(all(
+      (result$target_name == "RV_1of2_Vi99990016_po" & result$result == "positive") |
+      (result$target_name == "S. pneumoniae_Ba06439619_s1" & result$result == "negative")
+    ))
+  
+  })
