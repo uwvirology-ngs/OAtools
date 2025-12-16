33a435c6d11bc92a349d342f7a2cb99198c6e4cd
Aidan Shea <aidantshea@gmail.com>
2025-12-16 11:18:35 -0800

    feat: plot qc from se
    Re-implements the plot_qc function as plot_qc_from_se to support
    the SummarizedExperiment data structure.

--- R/functions-visualizations.R
+++ R/functions-visualizations.R
@@ -217,6 +217,48 @@ plot_qc <- function(data, batch = NA)
     return(fig)
 }
 
+
+#' Plot a 3D Quality Control Graphic from a SummarizedExperiment
+#' 
+#' Generates a 3-dimensional quality control plot comparing the amplification 
+#' status to the crt, Cq conf, and amplification score metrics output by 
+#' QuantStudio 12K Flex Software. 
+#'
+#' @param se a SummarizedExperiment object containing OpenArray qPCR data
+#'
+#' @returns a plotly figure
+#' 
+#' @export
+#'
+#' @examples
+plot_qc_from_se <- function(se) {
+    data <- as.data.frame(SummarizedExperiment::colData(se))
+        
+    data <- data |> dplyr::distinct(.data$well_position, .keep_all = TRUE)
+    
+    fig <- data |>
+        dplyr::mutate(crt = replace(.data$crt, is.na(.data$crt), 40)) |>
+        plotly::plot_ly(
+            x = ~.data$crt, y = ~.data$amp_score, z = ~.data$cq_conf,
+            color = ~.data$amp_status, colors = c('#BF382A', '#0C4B8E'),
+            text = paste(
+                data$batch_name, 
+                data$target_name, 
+                data$well_position, 
+                sep = "\n"
+            )
+        ) |>
+        plotly::add_markers() |>
+        plotly::layout(scene = list(
+            xaxis = list(title = 'Crt', range = c(0, 40)),
+            yaxis = list(title = 'Amp Score', range = c(0, 2)),
+            zaxis = list(title = 'Cq Conf', range = c(0, 1)))
+        ) |>
+        plotly::layout(title = unique(data$batch_name))
+    
+    return(fig)
+}
+
 #' Plot crt values
 #'
 #' This function generates a box and whisker plot visualizing the 
