8a34a0dc76845f1b1d3e0fb2521594e580d94869
Aidan Shea <aidantshea@gmail.com>
2025-12-31 12:59:47 -0800

    feat: improved ggplot2 figures
    Reworks the code for plotOverview() and plotCrt() for more consistent
    styling across the ggplot2 figures.

--- R/plotCrt.R
+++ R/plotCrt.R
@@ -1,59 +1,58 @@
-#' Plot Crt Values from SummarizedExperiment
+#' Plot Relative Cycle Threshold Values by Gene
 #' 
-#' Generates a box and whisker plot to visualize the distribution of observed
-#' Crt values for each assay target in an OpenArray plate. 
+#' Generates a box and whisker plot visualizing the distribution of Crt values
+#' measured on an OpenArray plate by Gene.
 #'
 #' @param se a SummarizedExperiment object containing OpenArray qPCR data
 #'
 #' @returns a ggplot2 figure
 #' 
+#' @import ggplot2
+#' 
+#' @importFrom rlang .data
+#' @importFrom SummarizedExperiment colData
+#' 
 #' @export
 #'
 #' @examples
 #' data(example_se)
+#' 
 #' plotCrt(example_se)
 plotCrt <- function(se) {
-    data <- as.data.frame(SummarizedExperiment::colData(se))
     
-    # collapse data frame for unique reactions
-    data <- data |>
-        dplyr::distinct(.data$well_position, .keep_all = TRUE)
+    # extract coldata
+    coldata <- as.data.frame(colData(se))
     
     # generate box and whisker plot
-    fig <- ggplot2::ggplot(data = data, mapping = ggplot2::aes(
-        x = .data$target_name, 
-        y = .data$crt, 
-        fill = .data$target_name
-    )) +
-        ggplot2::geom_boxplot(outlier.shape = NA, alpha = 0.8, na.rm = TRUE) +
-        ggplot2::geom_jitter(
-            position = ggplot2::position_jitter(width = 0.3, seed = 387),
-            size = 0.7,
-            alpha = 0.5,
-            color = "black",
+    fig <- coldata |> 
+        ggplot(data = coldata, mapping = aes(
+            x = .data$crt, 
+            y = .data$target_name,
+            fill = .data$target_name
+        )) +
+        scale_x_reverse(limits = c(40,0)) +
+        scale_y_discrete(
+            limits = rev(levels(factor(coldata$target_name)))
+        ) +
+        scale_fill_viridis_d(option = "C") +
+        geom_boxplot(alpha = 0.7, na.rm = TRUE) +
+        geom_point(
+            position = position_jitter(width = 0, height = 0.3, seed = 387),
             na.rm = TRUE
         ) +
-        ggplot2::scale_x_discrete(limits = rev(
-            levels(data$target_name)), 
-            drop = FALSE
+        labs(
+            title = "Crt Distribution by Gene",
+            x = "Crt",
+            y = "Gene"
         ) +
-        ggplot2::scale_y_reverse(limits = c(40,0)) +
-        ggplot2::scale_fill_viridis_d(option = "C", drop = FALSE) +
-        ggplot2::coord_flip() +
-        ggplot2::labs(
-            title = "Crt Distribution per Assay Target",
-            x = "Target",
-            y = "Crt"
-        ) +
-        ggplot2::theme_minimal(base_size = 12) +
-        ggplot2::theme(
-            axis.text.x = ggplot2::element_text(
-                angle = 45, 
-                hjust = 1, 
-                size = 8
-            ), 
-            legend.position = "none"
-        )
+        theme_minimal(base_size = 12) +
+        theme(
+            axis.text.x = element_text(size = 12),
+            axis.text.y = element_text(size = 12),
+            axis.title = element_text(size = 15),
+            legend.position = "none",
+            plot.title = element_text(size = 18, face = "bold", hjust = 0.5)
+        ) 
     
     return(fig)
 }
--- R/plotOverview.R
+++ R/plotOverview.R
@@ -1,22 +1,67 @@
-#' Plot Overview of Amplifications as a Heat Map
+#' Plot Amplification Status by Sample and Gene
 #' 
-#' This function generates a heat map summarizing the amplification status 
-#' results for each sample on the OpenArray plate by target gene. 
+#' Generates an overview graphic summarizing qPCR results for each combination 
+#' of sample and gene on an OpenArray experiment. 
 #'
 #' @param se a SummarizedExperiment object containing OpenArray qPCR data
 #'
 #' @returns a ggplot2 figure
 #' 
+#' @import ggplot2
+#' 
+#' @importFrom rlang .data
+#' @importFrom SummarizedExperiment colData
+#' 
 #' @export
 #'
 #' @examples
 #' data(example_se)
+#' 
 #' plotOverview(example_se)
 plotOverview <- function(se) {
     
-    # load in and format se to Data Frame
-    data <- as.data.frame(SummarizedExperiment::colData(se))
-    summary <- data |> 
+    # extract amplification status data from the SummarizedExperiment
+    summary <- .summarizeBySampleAndGene(se)
+    
+    # generate ggplot2 figure
+    fig <- summary |>
+        ggplot(mapping = aes(
+            x = .data$target_name, 
+            y = .data$sample_name, 
+            fill = .data$category
+        )) +
+        scale_y_discrete(
+            limits = rev(levels(factor(summary$sample_name)))
+        ) +
+        geom_tile(color = "black") +
+        scale_fill_manual(values = c(
+            "Negative"     = "white", 
+            "Inconclusive" = "blue", 
+            "Positive"     = "green"
+        )) +
+        labs(
+            title = "Amplification by Sample & Gene",
+            x = "Gene",
+            y = "Sample",
+            fill = "Highest Amp Status"
+        ) +
+        theme_minimal(base_size = 12) +
+        theme(
+            axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+            axis.text.y = element_text(size = 12),
+            axis.title = element_text(size = 15),
+            legend.text = element_text(size = 12),
+            legend.title = element_text(size = 15),
+            plot.title = element_text(size = 18, face = "bold", hjust = 0.5)
+        ) 
+    
+    return(fig)
+}
+
+# organize amplification data from SummarizedExperiment by sample and gene
+.summarizeBySampleAndGene <- function(se) {
+    
+    summary <- as.data.frame(colData(se)) |> 
         dplyr::group_by(.data$sample_name, .data$target_name) |> 
         dplyr::summarize(
             category = dplyr::case_when(
@@ -25,40 +70,10 @@ plotOverview <- function(se) {
                 TRUE ~ "Negative"
             ),
             .groups = "drop"
-        )
+        ) |> 
+        dplyr::mutate(category = factor(
+            .data$category, levels = c("Positive", "Inconclusive", "Negative")
+        ))
     
-    # generate heatmap figure
-    heatmap <- summary |>
-        ggplot2::ggplot(mapping = ggplot2::aes(
-            x = .data$target_name, 
-            y = .data$sample_name, 
-            fill = .data$category
-        )) +
-        ggplot2::geom_tile(color = "black") +
-        ggplot2::scale_fill_manual(values = c(
-            "Negative" = "white", 
-            "Inconclusive" = "blue", 
-            "Positive" = "green"
-        )) +
-        ggplot2::theme_minimal() +
-        ggplot2::labs(
-            title = "Amplification Status per Sample/Target Combination",
-            x = "Target",
-            y = "Sample",
-            fill = "Highest Amplification Status"
-        ) +
-        ggplot2::theme(
-            axis.text.x = ggplot2::element_text(
-                angle = 45, 
-                hjust = 1, 
-                size = 12
-            ),
-            axis.text.y = ggplot2::element_text(size = 12),
-            axis.title = ggplot2::element_text(size = 16),
-            plot.title = ggplot2::element_text(size = 20, face = "bold"),
-            legend.title = ggplot2::element_text(size = 16),
-            legend.text = ggplot2::element_text(size = 14)
-        )
-    
-    return(heatmap)
+    return(summary)
 }
