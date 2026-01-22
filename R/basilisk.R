2a19c2f68d6394d07edbc8987950a60cbaa85d9c
Aidan Shea <aidantshea@gmail.com>
2026-01-22 13:11:14 -0800

    fix: use compatible python3 version
    Reconfigures the basilisk.R setup to use python 3.12 instead of python
    3.13. Python 3.12 is the default python3 for Ubuntu 24.04 LTS.

--- R/basilisk.R
+++ R/basilisk.R
@@ -3,10 +3,10 @@ OAtools_env <- basilisk::BasiliskEnvironment(
     envname = "conda_env",
     pkgname = "OAtools",
     packages = c(
-        "python=3.13.5", 
-        "numpy=2.3.2", 
-        "pandas=2.3.1", 
-        "scipy=1.16.1"
+        "python=3.12", 
+        "numpy==2.4.1", 
+        "pandas==2.3.3", 
+        "scipy==1.17.0"
     ),
     channels = c("conda-forge")
 )
