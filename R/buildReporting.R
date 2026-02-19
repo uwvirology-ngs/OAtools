6f9ed4eb6929e70ddc398b2b9edb62f117eceee2
Aidan Shea <aidantshea@gmail.com>
2026-02-18 17:27:16 -0800

    feat: export run data as .xlsx from Shiny app
    Adds the ability to export run data in excel format from the Shiny UI.

--- R/buildReporting.R
+++ R/buildReporting.R
@@ -8,25 +8,25 @@
 .uiReporting <- function(id) {
     ns <- NS(id)
     tabPanel("Reporting",
-        titlePanel("Export the Experimental Results"),
-        sidebarLayout(
-            sidebarPanel(
-                p("Download Run Report (HTML)"),
-                downloadButton(
-                    outputId = ns("download_report"),
-                    label = "download report"
-                ),
-                p(),
-                p("Download Run Data (.xlsx)"),
-                downloadButton(
-                    outputId = ns("download_csv"),
-                    label = "download run data"
-                )
-            ),
-            mainPanel(
-                # pass
-            )
-        )
+             titlePanel("Export the Experimental Results"),
+             sidebarLayout(
+                 sidebarPanel(
+                     p("Download PCR Report (HTML)"),
+                     downloadButton(
+                         outputId = ns("download_report"),
+                         label = "download report"
+                     ),
+                     p(),
+                     p("Download Run Data (.xlsx)"),
+                     downloadButton(
+                         outputId = ns("download_xlsx"),
+                         label = "download data"
+                     )
+                 ),
+                 mainPanel(
+                     
+                 )
+             )
     )
 }
 
@@ -41,21 +41,38 @@
 #' 
 #' @importFrom S4Vectors metadata
 #' @importFrom rlang .data
+#' @importFrom SummarizedExperiment colData
 .serverReporting <- function(id, se) {
     moduleServer(id, function(input, output, session) {
         
+        # download HTML PCR report
         output$download_report <- downloadHandler(
             filename = function() {
                 oa_plate_id <- metadata(se())$run_info |> 
                     dplyr::filter(.data$Field == "Experiment Name") |> 
                     dplyr::pull("Data")
-                paste0("report_", oa_plate_id, "_", Sys.Date(), ".html") 
+                paste0("Report_", oa_plate_id, "_", Sys.Date(), ".html") 
             },
             content = function(file) {
                 req(se())
                 tempfile <- tempdir()
                 generateReport(se(), path = tempfile)
                 file.copy(file.path(tempfile, "report.html"), file)
+            }
+        )
+        
+        # download Excel run data
+        output$download_xlsx <- downloadHandler(
+            filename = function() {
+                oa_plate_id <- metadata(se())$run_info |> 
+                    dplyr::filter(.data$Field == "Experiment Name") |> 
+                    dplyr::pull("Data")
+                paste0("OAtools_", oa_plate_id, "_", Sys.Date(), ".xlsx") 
+            },
+            content = function(file) {
+                req(se())
+                oatools_data <- as.data.frame(colData(se()))
+                writexl::write_xlsx(oatools_data, path = file)
             }
         )
     })
