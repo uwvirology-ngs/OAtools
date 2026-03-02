f541402fa79cd356a4764cb31a96642f4f0f8e76
Aidan Shea <aidantshea@gmail.com>
2026-03-02 15:01:22 -0800

    refactor: include r_squared in PCR results
    Includes regression_type and r_squared in the subset of metadata copied
    from fit models to the experiment colData by determinePCRResults().
    Also refactors the determinePCRResults() function for modularity and
    readability.

--- R/determinePCRResults.R
+++ R/determinePCRResults.R
@@ -1,17 +1,17 @@
-#' Determine PCR results from features of multicomponent fluorescence curves
+#' Determine PCR results from fit curves
 #' 
-#' Assigns positive or negative PCR results to every PCR reaction depending on
-#' the equation of the model curve optimized to the fluorescence curve. In 
-#' particular, the reporter dye fluorescence without normalization is used to 
-#' distinguish between real amplification and false positives. 
+#' Assigns positive or negative PCR results to each reaction depending on the
+#' equation of the model optimized to observed multicomponent fluorescence 
+#' values. The reporter dye fluorescence without normalization is used to 
+#' distinguish between real amplification and false positives.
 #' 
 #' @details
-#' The target-threshold key is an Excel file which stores threshold values 
-#' used to separate PCR curves. These thresholds are defined across metrics 
-#' such as cycle threshold, overall change-in-fluorescence, and slope of the 
-#' fluorescence curve during the exponential phase. The thresholds are 
-#' separately defined for each gene to accommodate inherent differences in
-#' assays.
+#' The key is an Excel file storing values used by `determinePCRResults()` to 
+#' categorize PCR curves as `positive` or `negative`. In the key, each gene is 
+#' associated with threshold values for Crt, change-in-fluorescence, and slope
+#' at the reaction midpoint.
+#' 
+#' For more specifics, input `?target_threshold_key` into the R console. 
 #'
 #' @param se a SummarizedExperiment object containing OpenArray qPCR Data
 #' @param key_path file path to the target-threshold key
@@ -21,6 +21,9 @@
 #' @import SummarizedExperiment
 #' 
 #' @importFrom rlang .data
+#' @importFrom readxl read_excel
+#' @importFrom S4Vectors DataFrame
+#' @importFrom purrr map_dbl map_chr
 #' 
 #' @export
 #'
@@ -37,32 +40,52 @@
 #'     determinePCRResults(key_path = key_path)
 determinePCRResults <- function(se, key_path) {
     
-    # import target-threshold key
-    key <- readxl::read_excel(path = key_path, na = "NA") |> 
+    # pull critical curve fitting metadata and append to colData
+    models <- metadata(se)$fluo_reporter_models
+    
+    data <- as.data.frame(colData(se)) |>
+        dplyr::mutate(regression_type = map_chr(
+            paste0("well_", .data$well),
+            function(well) { models[[well]]$regression_type }
+        )) |> 
+        dplyr::mutate(r_squared = map_dbl(
+            paste0("well_", .data$well),
+            function(well) { models[[well]]$r_squared }
+        )) |> 
+        dplyr::mutate(delta_fluo = map_dbl(
+            paste0("well_", .data$well), 
+            function(well) { models[[well]]$delta_y }
+        )) |> 
+        dplyr::mutate(midpoint_slope = map_dbl(
+            paste0("well_", .data$well), 
+            function(well) { models[[well]]$slope_midpoint }
+        ))
+
+    # merge experiment colData with thresholds defined by key
+    key <- read_excel(path = key_path, na = "NA") |> 
         janitor::clean_names() |> 
         dplyr::mutate(target = as.factor(.data$target))
     
-    # load in models
-    models <- metadata(se)$fluo_reporter_models
+    data <- data |> dplyr::left_join(key, by = c("target_name" = "target"))
     
-    # merge existing coldata with thresholds and model features
-    data <- as.data.frame(colData(se)) |>
-        dplyr::left_join(key, by = c("target_name" = "target")) |> 
-        dplyr::mutate(midpoint_slope = purrr::map_dbl(
-            paste0("well_", .data$well), 
-            function(well) { models[[well]]$slope_midpoint }
-        )) |> 
-        dplyr::mutate(delta_fluo = purrr::map_dbl(
-            paste0("well_", .data$well), 
-            function(well) { models[[well]]$delta_y }
-        ))
+    # determine PCR results and append to the colData
+    results <- .applyResults(data)    
+    colData(se) <- DataFrame(results)
     
-    # apply the rules of the key and append the results to the tibble
+    return(se)
+}
+
+# adds PCR results to the tibble, applying thresholds defined by the key
+.applyResults <- function(data) {
+    
+    # add pass/fail cols for crt, midpoint slope, and change-in-fluorescence,
+    # then require positive results to pass on all three metrics. if no 
+    # threshold values are defined at all, result is TBD.
     results <- data |>
         dplyr::mutate(
             crt_pass   = is.na(.data$crt_threshold) | 
                 (!is.na(.data$crt) & .data$crt < .data$crt_threshold),
-            
+
             slope_pass = is.na(.data$slope_threshold) | 
                 .data$midpoint_slope > .data$slope_threshold,
             
@@ -80,7 +103,5 @@ determinePCRResults <- function(se, key_path) {
             )
         )
     
-    SummarizedExperiment::colData(se) <- S4Vectors::DataFrame(results)
-    
-    return(se)
+    return(results)
 }
